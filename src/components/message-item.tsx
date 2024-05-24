890a1b763fd031ed406d6b7e14cd55894d1c3035
aldotestino <aldotestino4@gmail.com>
2024-05-24 21:00:23 +0200

    add remark

--- src/components/message-item.tsx
+++ src/components/message-item.tsx
@@ -2,6 +2,7 @@ import { cn, formatDate } from '@/lib/utils';
 import { Message } from 'ai';
 import React from 'react';
 import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 function MessageItem({
   message
@@ -9,9 +10,9 @@ function MessageItem({
   message: Message
 }) {
   return (
-    <div className={cn('flex gap-1 flex-col w-fit max-w-xs', message.role === 'user' ? 'self-end items-end' : 'self-start items-start')}>
-      <div className={cn('prose prose-slate dark:prose-invert p-2 rounded-xl w-fit', message.role === 'user' ? 'bg-primary text-primary-foreground rounded-br-none' : 'bg-secondary text-secondary-foreground rounded-bl-none')}>
-        <Markdown>{message.content}</Markdown>
+    <div className={cn('flex gap-1 flex-col w-fit max-w-md', message.role === 'user' ? 'self-end items-end' : 'self-start items-start')}>
+      <div className={cn('prose prose-slate dark:prose-invert prose-a:text-primary hover:prose-a:text-primary/80 p-2 rounded-xl w-fit', message.role === 'user' ? 'bg-primary text-primary-foreground rounded-br-none' : 'bg-secondary text-secondary-foreground rounded-bl-none')}>
+        <Markdown remarkPlugins={[remarkGfm]}>{message.content}</Markdown>
       </div>
       <small className="text-muted-foreground">{formatDate(message.createdAt!)}</small>
     </div>
