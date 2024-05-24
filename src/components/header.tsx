a1f2edc913c6f9bc3d8b94bd1748eeb123e29a1b
aldotestino <aldotestino4@gmail.com>
2024-05-24 13:41:01 +0200

    header + dark mode

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+import Logo from './logo';
+import ThemeButton from './theme-button';
+
+function Header() {
+  return (
+    <header className="w-full flex items-center justify-between py-4">
+      <div className='flex items-center gap-2'>
+        <Logo className='w-6 h-auto fill-current' />
+        <h1 className='text-2xl font-semibold'>ollama-chat</h1>
+      </div>
+      <ThemeButton />
+    </header>
+  );
+}
+
+export default Header;
