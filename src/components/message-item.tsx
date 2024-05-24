a80a3d8d4078aaf45d23736af984f7ce6ec0adad
aldotestino <aldotestino4@gmail.com>
2024-05-24 20:55:47 +0200

    add markdown and prose

--- src/components/message-item.tsx
+++ src/components/message-item.tsx
@@ -1,6 +1,7 @@
 import { cn, formatDate } from '@/lib/utils';
 import { Message } from 'ai';
 import React from 'react';
+import Markdown from 'react-markdown';
 
 function MessageItem({
   message
@@ -9,9 +10,9 @@ function MessageItem({
 }) {
   return (
     <div className={cn('flex gap-1 flex-col w-fit max-w-xs', message.role === 'user' ? 'self-end items-end' : 'self-start items-start')}>
-      <p className={cn('p-2 rounded-xl w-fit', message.role === 'user' ? 'bg-primary text-primary-foreground rounded-br-none' : 'bg-secondary text-secondary-foreground rounded-bl-none')}>
-        {message.content}
-      </p>
+      <div className={cn('prose prose-slate dark:prose-invert p-2 rounded-xl w-fit', message.role === 'user' ? 'bg-primary text-primary-foreground rounded-br-none' : 'bg-secondary text-secondary-foreground rounded-bl-none')}>
+        <Markdown>{message.content}</Markdown>
+      </div>
       <small className="text-muted-foreground">{formatDate(message.createdAt!)}</small>
     </div>
   );
